87f935b4b8aadbca4f44b4692f9dd205b59d813c
ihozh <1292496376@qq.com>
2017-06-21 23:41:13 -0400

    add src folder and cuboid3D.py

---
+++ src/cuboid3D.py
@@ -0,0 +1,41 @@
+"""
+cuboid3D can draw cuboid in 3d scene
+
+how to draw:
+
+cuboid3D.plotCubeAt(pos=(0,0,0),size=(1,1,1),ax=ax)
+
+"""
+
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cuboid_data(pos, size):
+    # code taken from
+    # https://stackoverflow.com/a/35978146/4124317
+    # suppose axis direction: x: to left; y: to inside; z: to upper
+    # get the (left, outside, bottom) point
+    o = [a - b / 2 for a, b in zip(pos, size)]
+    # get the length, width, and height
+    l, w, h = size
+    x = [[o[0], o[0] + l, o[0] + l, o[0], o[0]],
+         [o[0], o[0] + l, o[0] + l, o[0], o[0]],
+         [o[0], o[0] + l, o[0] + l, o[0], o[0]],
+         [o[0], o[0] + l, o[0] + l, o[0], o[0]]]
+    y = [[o[1], o[1], o[1] + w, o[1] + w, o[1]],
+         [o[1], o[1], o[1] + w, o[1] + w, o[1]],
+         [o[1], o[1], o[1], o[1], o[1]],
+         [o[1] + w, o[1] + w, o[1] + w, o[1] + w, o[1] + w]]
+    z = [[o[2], o[2], o[2], o[2], o[2]],
+         [o[2] + h, o[2] + h, o[2] + h, o[2] + h, o[2] + h],
+         [o[2], o[2], o[2] + h, o[2] + h, o[2]],
+         [o[2], o[2], o[2] + h, o[2] + h, o[2]]]
+    return x, y, z
+
+def plotCubeAt(pos=(0,0,0),size=(1,1,1),ax=None):
+    # Plotting a cube element at position pos
+    if ax !=None:
+        X, Y, Z = cuboid_data( pos, size )
+        ax.plot_surface(X, Y, Z, color='b', rstride=1, cstride=1, alpha=1)
+
